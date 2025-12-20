baa85ac59597d96834bdac6543120dd67d2d484f
1372594487 <1372594487@qq.com>
2025-12-20 18:31:23 +0800

    feat: 补充struct知识点

--- day2/09-OOP/01-struct/struct.go
+++ day2/09-OOP/01-struct/struct.go
@@ -1,15 +1,28 @@
 /*
  * @Author: zywOo 1372594487@qq.com
  * @Date: 2025-07-02 16:00:47
- * @LastEditors: zywOo 1372594487@qq.com
- * @LastEditTime: 2025-07-02 16:05:38
+ * @LastEditors: 1372594487 1372594487@qq.com
+ * @LastEditTime: 2025-12-20 18:22:49
  * @FilePath: \go-learning\day2\08-struct\struct.go
  * @Description: struct定义与使用
+ 应用场景：
+1、用户管理系统：在电商平台、社交应用等系统中，需要管理用户信息，如用户名、密码、年龄、性别、地址等。
+可以使用结构体来表示用户信息，每个用户都是一个结构体实例，包含用户的各种属性。
+2、
  *
  */
 package main
 
 import "fmt"
+
+//结构体是Go语言中组织相关数据的复合类型，将多个不同类型的数据组合在一起形成的数据类型
+// 结构体是值类型，在函数间传递时，会复制一份新的结构体，而不是传递指针
+
+// 结构体初始化方式
+// var p Person //各字段为零值
+// person :=Person{name: "张三", age: 18} //结构体字面量
+// new(Person) //返回指针
+
 
 // type关键字定义结构体
 type Person struct {
@@ -17,39 +30,48 @@ type Person struct {
 	age  int
 }
 
-func changeName(p Person) {
-	p.name = "李四"
+
+type User struct{
+	ID int
+	UserName string
+	Password string
+	Email string
+	Age int
+	Address Address
 }
 
-type Person2 struct {
-	name string
-	age  int
+type Address struct{
+	Province string
+	City string
+	Street string
+	ZipCode string
+	Detail string
 }
 
-func changeName2(p *Person2, name string, age int) {
-	// Go语言中的逻辑或运算符||不能用于赋值操作。Go语言也不支持三元运算符
-	if name != "" {
-		p.name = name
-	}
-	if age != 0 {
-		p.age = age
-	}
+type UserProfile struct {
+	UserName string
+	Age int
+	Email string
+	Address
 }
 
 func main() {
-	var p Person
-	p.name = "张三"
-	p.age = 18
-	fmt.Println(p)
-	fmt.Printf("p的类型是：%T\n", p)
-	changeName(p)
-	fmt.Println(p)
-	/* ----------------------------------------------------------------------- */
-	fmt.Println("-------------------------")
-	var p2 Person2
-	p2.name = "王五"
-	p2.age = 18
-	fmt.Println(p2)
-	changeName2(&p2, "赵六", 20)
-	fmt.Println(p2)
+	addr := Address{Province: "北京市", City: "北京市", Street: "朝阳区", ZipCode: "100000", Detail: "xx街道xx号"}
+	user1 := User{ID: 1, UserName: "张三", Password: "123456", Email: "123456@qq.com", Age: 18, Address: addr}
+	user2 := User{ID: 2, UserName: "李四", Password: "123456", Email: "123456@qq.com", Age: 18, Address: Address{
+		Province: "广西省", City: "南宁市", Street: "武鸣区", ZipCode: "000000", Detail: "xx街道xx号",
+	}}
+	user3 := &User{ID: 3, UserName: "王五", Password: "123456", Email: "123456@qq.com", Age: 18, Address: Address{
+	 Province: "广东省", City: "广州市", Street: "天河区", ZipCode: "000000", Detail: "xx街道xx号",
+	}
+	}
+
+	UserProfile := UserProfile{UserName: "赵六", Age: 18, Email: "123456@qq.com", Address: Address{
+		Province: "广东省", City: "广州市", Street: "天河区", ZipCode: "000000", Detail: "xx街道xx号",
+	}}
+
+	fmt.Printf("User1: %+v\n,City: %s\n,Street: %s\n", user1, user1.Address.City, user1.Address.Street)
+	fmt.Printf("User1: %+v\n,City: %s\n,Street: %s\n", user2, user2.Address.City, user2.Address.Street)
+	fmt.Printf("User3: %+v\n,City: %s\n,Street: %s\n",user3, user3.Address.City, user3.Address.Street)
+	fmt,Printf("UserProfile: %+v\n,City: %s\n,Street: %s\n",UserProfile, UserProfile.City, UserProfile.Street)
 }
