58065e5bfafb0aea5583b60333b91f382c4cb5d4
1372594487 <1372594487@qq.com>
2025-12-28 01:22:43 +0800

    feat:补充GO标准库：text/template、compress/gzip、bufio

---
+++ day2/11-Go标准库/main.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: 1372594487 1372594487@qq.com
+ * @Date: 2025-12-28 00:57:53
+ * @LastEditors: '1372594487
+ * @LastEditTime: 2025-12-28 00:58:09
+ * @Description: File description
+ */
+package main
+
+import "go-standard-library/other"
+
+func main() {
+	// other.Demotemplate()
+	// other.DemoCompression()
+	other.DemoBuffIO()
+}
